8227a4468b55675ebacdc2406d22e12cd050b00c
olezhokmer2002 <kenzoyamazaki2002@gmail.com>
2021-11-18 22:34:34 +0200

    ex10 done

--- ex10-workerpool/goroutines.go
+++ ex10-workerpool/goroutines.go
@@ -1,4 +1,36 @@
 package goroutines
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"time"
+)
+
+func worker(id int, jobs <-chan float64) {
+	fmt.Printf("worker:%d spawning\n", id)
+	for j := range jobs {
+		fmt.Printf("worker:%d sleep:%.1f\n", id, j)
+		time.Sleep(time.Duration(float64(time.Second.Nanoseconds()) * j))
+	}
+	fmt.Printf("worker:%d stopping\n", id)
+}
+
 func Run(poolSize int) {
+	jobs := make(chan float64, poolSize)
+	id := 0
+
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		i, _ := strconv.ParseFloat(s.Text(), 64)
+
+		jobs <- i
+		if id < poolSize {
+			id++
+			go worker(id, jobs)
+		}
+	}
+	close(jobs)
 }
